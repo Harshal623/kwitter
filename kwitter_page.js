5126ee020920461e6b69d1e9e98471e823617cda
Harshal623 <89270049+Harshal623@users.noreply.github.com>
2022-04-30 09:23:11 -0400

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -29,18 +29,36 @@ function getData() {
             snapshot.forEach(function (childSnapshot) {
                   childKey = childSnapshot.key;
                   childData = childSnapshot.val();
-                  if (childKey != "purpose") {
+                  if (childKey != "porpose") {
                         firebase_message_id = childKey;
                         message_data = childData;
                         //Start code
-
+                        console.log(firebase_message_id)
+                        console.log(message_data)
+                        new_name = message_data["new_name"]
+                        message = message_data["message"]
+                        like = message_data["like"]
+                        name_tag = "<h4>" + new_name + "<img src = 'tick.png' class = 'user_tick'></h4>"
+                        message_tag = "<h4 class = 'message_h4'>" + message + "</h4>"
+                        like_button = "<button class = 'btn btn-warning' id = '"+firebase_message_id+"' value = '"+like+"' onclick = 'updatelikes(this.id)'>"
+                        span_tag = "<span class = 'glyphicon glyphicon-thumbs-up'>Like:" + like + "</span></button><hr>"
+                        row = name_tag + message_tag + like_button + span_tag
+                        document.getElementById("output").innerHTML += row
+                  
                         //End code
                   }
             });
       });
 }
 getData();
-
+function updatelikes(message_id){
+      console.log(message_id)
+      likes = document.getElementById(message_id).value
+      updatedlikes = Number(likes) + 1
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updatedlikes
+      });
+}
 function logout(){
       localStorage.removeItem("user")
       localStorage.removeItem("Room_name")
